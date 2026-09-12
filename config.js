4cfc9a1699d422391efa1a9ed5b8af087177651c
stevetheturtlestories-png <stevetheturtlestories@gmail.com>
2026-09-12 12:52:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ window.CHOICEGRADE_CONFIG = {
   supabaseUrl: "https://xaycufxjsklisidvufsr.supabase.co",
   supabaseAnonKey: "sb_publishable_EmRhJM2Ih9Iiffc5BxhEsQ_CVMPDrwN",
   checkoutEndpoint: "https://choice-grade.vercel.app/api/create-checkout"
+};
